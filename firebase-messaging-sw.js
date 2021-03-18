a6f444256f2b4c2a880c34a693ee110578b5fc67
axel-gonzalez-121 <axel.gonzalez@greenmarimba.io>
2021-03-18 14:19:05 -0600

    Route change

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -3,7 +3,7 @@ importScripts('https://www.gstatic.com/firebasejs/8.2.10/firebase-messaging.js')
 
 if ('serviceWorker' in navigator) {
 	// navigator.serviceWorker.register('../firebase-messaging-sw.js')
-	navigator.serviceWorker.register('../test-pwa/firebase-messaging-sw.js')
+	navigator.serviceWorker.register('/test-pwa/firebase-messaging-sw.js')
 		.then(function (registration) {
 			console.log('Registration successful, scope is:', registration.scope);
 		}).catch(function (err) {
