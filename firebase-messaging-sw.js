4722ebbd34e841615fda9ad76b7ff02891177758
axel-gonzalez-121 <axel.gonzalez@greenmarimba.io>
2021-03-18 11:43:44 -0600

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -2,14 +2,14 @@ importScripts('https://www.gstatic.com/firebasejs/8.2.10/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/8.2.10/firebase-messaging.js');
 
 // Prueba
-if ('serviceWorker' in navigator) {
-	navigator.serviceWorker.register('../firebase-messaging-sw.js')
-		.then(function (registration) {
-			console.log('Registration successful, scope is:', registration.scope);
-		}).catch(function (err) {
-			console.log('Service worker registration failed, error:', err);
-		});
-}
+// if ('serviceWorker' in navigator) {
+// 	navigator.serviceWorker.register('../firebase-messaging-sw.js')
+// 		.then(function (registration) {
+// 			console.log('Registration successful, scope is:', registration.scope);
+// 		}).catch(function (err) {
+// 			console.log('Service worker registration failed, error:', err);
+// 		});
+// }
 
 const firebaseConfig = {
 	apiKey: "AIzaSyBUAXUSbXSLoX7baq3RqNVRCCM_MwwlTKA",
@@ -23,3 +23,25 @@ const firebaseConfig = {
 
 firebase.initializeApp(firebaseConfig);
 const messaging = firebase.messaging();
+
+// messaging.setBackgroundMessageHandler(function(payload) {
+// 	const promiseChain = clients
+// 	  .matchAll({
+// 		type: "window",
+// 		includeUncontrolled: true
+// 	  })
+// 	  .then(windowClients => {
+// 		for (let i = 0; i < windowClients.length; i++) {
+// 		  const windowClient = windowClients[i];
+// 		  windowClient.postMessage(payload);
+// 		}
+// 	  })
+// 	  .then(() => {
+// 		return registration.showNotification("my notification title");
+// 	  });
+// 	return promiseChain;
+//   });
+//   self.addEventListener('notificationclick', function(event) {
+// 	// do what you want
+// 	// ...
+//   });
