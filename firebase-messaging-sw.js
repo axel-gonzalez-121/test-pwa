d417c610b37b5ce7f1d804ab96fb123918bb2e17
axel-gonzalez-121 <axel.gonzalez@greenmarimba.io>
2021-03-17 19:10:20 -0600

    Test

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,25 @@
+importScripts('https://www.gstatic.com/firebasejs/8.2.10/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/8.2.10/firebase-messaging.js');
+
+// Prueba
+if ('serviceWorker' in navigator) {
+	navigator.serviceWorker.register('../firebase-messaging-sw.js')
+		.then(function (registration) {
+			console.log('Registration successful, scope is:', registration.scope);
+		}).catch(function (err) {
+			console.log('Service worker registration failed, error:', err);
+		});
+}
+
+const firebaseConfig = {
+	apiKey: "AIzaSyBUAXUSbXSLoX7baq3RqNVRCCM_MwwlTKA",
+	authDomain: "push-test-8543c.firebaseapp.com",
+	projectId: "push-test-8543c",
+	storageBucket: "push-test-8543c.appspot.com",
+	messagingSenderId: "222177857579",
+	appId: "1:222177857579:web:b71df4c74397f5d79c1a6e",
+	measurementId: "G-7M0X3NYC1N"
+};
+
+firebase.initializeApp(firebaseConfig);
+const messaging = firebase.messaging();
